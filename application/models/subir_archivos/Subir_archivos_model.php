b254abbec1fb38131c68d1056a7d14c0b06726f5
Rajome06 <elmer.guardado@gmail.com>
2022-11-08 17:04:53 -0600

    Correccion de consulta no clasificados

--- application/models/subir_archivos/Subir_archivos_model.php
+++ application/models/subir_archivos/Subir_archivos_model.php
@@ -15,18 +15,16 @@ class Subir_archivos_model extends CI_Model
       
         if (isset($no_identificacion)) {
             if ($opcion == 1) {
-              
                 $query = $this->db
-                                      ->select("d.id , d.codigo_producto, d.descripcion,   d.num_factura , d.tlc , d.cuantia")
-                                      ->join('duarx.dpr as d', 'd.id_file =  f.id', 'inner')
-                                      ->join('duarx.producto_importador as du', "du.codproducto = d.codigo_producto ")
-                                      ->where('d.partida IS  NULL')
-                                      ->where('f.id', $id_file)
-                                      ->where('du.importador', $no_identificacion)
-                                      ->where('d.pais_id',  $_SESSION['pais_id'])
-                                      ->order_by('d.num_factura, d.id')
-                                      ->get('gacela.file as f')
-                                     ->result();
+                ->select("d.id , d.codigo_producto, d.descripcion,   d.num_factura , d.tlc , d.cuantia")
+                ->join('duarx.dpr as d', 'd.id_file =  f.id', 'inner')
+                ->join('duarx.producto_importador as du', "du.codproducto = d.codigo_producto and du.importador = '$no_identificacion'", 'left')
+                ->where('du.partida IS  NULL')
+                ->where('f.id', $id_file)
+                ->where('d.pais_id',  $_SESSION['pais_id'])
+                ->order_by('d.num_factura, d.id')
+                ->get('gacela.file as f')
+               ->result();
             } elseif ($opcion == 2) {
                
                 /* d.num_factura as 'Numero Factura' , d.tlc as TLC, f.c807_file as 'File' , d.cuantia, d.proveedor ")*/
